c3dfdfe537ca109ff8be2203cb210119e8f7d5a9
Thomas Tortorini <thomastortorini@gmail.com>
2018-12-05 03:20:42 +0100

    api/common/sendJSON

---
+++ api/common/sendJSON.php
@@ -0,0 +1,15 @@
+<?php
+
+function sendJSON( $httpcode, $data = null ) {
+	$obj = ( object )[
+		'ok' => $httpcode >= 200 && $httpcode < 300,
+		'data' => $data ?? ( object )[],
+	];
+	if ( is_string( $data ) ) {
+		$obj->msg = $data;
+		unset( $obj->data );
+	}
+	header( 'Content-Type: application/json; charset=utf-8' );
+	http_response_code( $httpcode );
+	die( json_encode( $obj ) );
+}
