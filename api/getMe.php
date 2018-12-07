691e3f174cd044bfc7cdb91a1dbb59b6710936cc
Thomas Tortorini <thomastortorini@gmail.com>
2018-12-07 05:53:05 +0100

    getMe

---
+++ api/getMe.php
@@ -0,0 +1,17 @@
+<?php
+
+error_reporting( -1 );
+
+require_once( 'common/sendJSON.php' );
+require_once( 'common/enableCors.php' );
+
+enableCors();
+
+session_start();
+$me = $_SESSION[ 'me' ] ?? null;
+
+if ( $me ) {
+	sendJSON( 200, $me );
+} else {
+	sendJSON( 401 );
+}
