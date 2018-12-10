342772d0c8d81652d9e2c5ba52659aed79bd2d1b
Thomas Tortorini <thomastortorini@gmail.com>
2018-12-10 04:09:13 +0100

    api/common/sendEmail: update the code to PHP7.2

--- api/common/sendEmail.php
+++ api/common/sendEmail.php
@@ -1,9 +1,9 @@
 <?php
 
 function sendEmail( $email, $title, $msg ) {
-	$head = "From: GridSound <contact@gridsound.com>\r\n" .
-		"Reply-To: contact@gridsound.com\r\n" .
-		'X-Mailer: PHP/' . phpversion();
-
-	return mail( $email, $title, $msg, $head );
+	return mail( $email, $title, $msg, array(
+		'From' => 'GridSound <contact@gridsound.com>',
+		'Reply-To' => 'contact@gridsound.com',
+		'X-Mailer' => 'PHP/' . phpversion(),
+	) );
 }
