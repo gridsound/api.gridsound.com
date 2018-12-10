3a1c123b217c47dafd26e604af250c5b65016642
Thomas Tortorini <thomastortorini@gmail.com>
2018-12-10 04:12:55 +0100

    api/common/sendEmailConfirmation

---
+++ api/common/sendEmailConfirmation.php
@@ -0,0 +1,10 @@
+<?php
+
+function sendEmailConfirmation( $id, $username, $email, $code ) {
+	return sendEmail( $email, 'Email confirmation',
+		"Hi $username,\r\n\r\n" .
+		"Welcome to GridSound !\r\n" .
+		"Clicking that link will confirm your email :\r\n" .
+		"https://api.gridsound.com/verify?id=$id&data=$email&code=$code"
+	);
+}
