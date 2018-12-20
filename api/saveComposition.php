774cf0a3ea1004695e28326c76d2014ed8861982
Thomas Tortorini <thomastortorini@gmail.com>
2018-12-20 20:21:44 +0100

    api/saveComposition

---
+++ api/saveComposition.php
@@ -0,0 +1,39 @@
+<?php
+
+error_reporting( -1 );
+
+require_once( 'common/sendJSON.php' );
+require_once( 'common/enableCors.php' );
+
+enableCors();
+session_start();
+
+$me = $_SESSION[ 'me' ] ?? null;
+$_POST = json_decode( file_get_contents( 'php://input' ), true );
+$POSTcmp = json_decode( $_POST[ 'composition' ] ?? null );
+
+if ( !$me ) {
+	sendJSON( 401 );
+} else if ( $me->user->status === 'EMAIL_TO_VERIFY' ) {
+	sendJSON( 401, 'email-not-verified' );
+} else if ( $me->user->status !== 'NORMAL' ) {
+	sendJSON( 403 );
+} else if ( !$POSTcmp ) {
+	sendJSON( 400 );
+}
+
+require_once( 'common/connection.php' );
+
+$id = $POSTcmp->id;
+$iduser = $me->user->id;
+$data = json_encode( $POSTcmp );
+$res = $mysqli->query( "INSERT INTO `compositions` (
+	`id`,  `iduser`,  `data`, `created`, `updated` ) VALUES (
+	'$id', '$iduser', '$data', NOW(),     NOW() )" );
+
+if ( $res ) {
+	$mysqli->close();
+	sendJSON( 200 );
+} else {
+	sendJSON( 500, $mysqli->error );
+}
