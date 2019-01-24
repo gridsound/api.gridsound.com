7d4731d0d2076076de214c7232ca44a1435516c8
Thomas Tortorini <thomastortorini@gmail.com>
2019-01-24 21:13:17 +0100

    api/resetPassword

---
+++ api/resetPassword.php
@@ -0,0 +1,50 @@
+<?php
+
+error_reporting( -1 );
+
+require_once( 'common/sendJSON.php' );
+require_once( 'common/enableCors.php' );
+
+enableCors();
+
+$_POST = json_decode( file_get_contents( 'php://input' ), true );
+$POSTcode = $_POST[ 'code' ] ?? null;
+$POSTpass = $_POST[ 'pass' ] ?? '';
+$POSTemail = $_POST[ 'email' ] ?? null;
+
+if ( !$POSTcode ) {
+	sendJSON( 400 );
+} if ( !filter_var( $POSTemail, FILTER_VALIDATE_EMAIL ) ) {
+	sendJSON( 400, 'email:bad-format' );
+} if ( mb_strlen( $POSTpass ) < 6 ) {
+	sendJSON( 400, 'pass:too-short' );
+}
+
+require_once( 'common/connection.php' );
+require_once( 'common/sendEmail.php' );
+
+$code = $mysqli->real_escape_string( $POSTcode );
+$email = $mysqli->real_escape_string( $POSTemail );
+
+$res = $mysqli->query( "DELETE FROM `passwordForgotten` WHERE
+	`email` = '$email' AND
+	`code` = '$code'" );
+if ( !$res ) {
+	sendJSON( 500, $mysqli->error );
+} if ( $mysqli->affected_rows < 1 ) {
+	sendJSON( 403, 'password:bad-code' );
+}
+
+$pass = password_hash( $POSTpass, PASSWORD_BCRYPT );
+$res = $mysqli->query( "UPDATE `users` SET `pass` = '$pass' WHERE `email` = '$email'" );
+
+if ( !$res ) {
+	sendJSON( 500, $mysqli->error );
+} if ( $mysqli->affected_rows > 0 ) {
+	sendEmail( $email, 'Password changed',
+		"Hi,\r\n\r\n" .
+		"Your password has been reset, you can now log in again on " .
+		"https://gridsound.github.io"
+	);
+	sendJSON( 200 );
+}
