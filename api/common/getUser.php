8e7d7cf21f8e1167aac8148728747ae11dba9c72
Thomas Tortorini <thomastortorini@gmail.com>
2019-12-15 05:50:44 +0100

    api/common/getUser: check emailchecked too

--- api/common/getUser.php
+++ api/common/getUser.php
@@ -1,9 +1,9 @@
 <?php
 
 function getUser( &$mysqli, $idType, $id, $full = false ) {
-	$fields = '`id`, `email`, `emailpublic`, `firstname`, `lastname`, `username`, `avatar`';
+	$fields = '`id`, `email`, `emailpublic`, `emailchecked`, `firstname`, `lastname`, `username`, `avatar`';
 	if ( $full ) {
-		$fields .= ', `pass`, `emailchecked`';
+		$fields .= ', `pass`';
 	}
 	$id = $mysqli->real_escape_string( $id );
 	$idType = $mysqli->real_escape_string( $idType );
@@ -17,9 +17,12 @@ function getUser( &$mysqli, $idType, $id, $full = false ) {
 			$user = false;
 		} else {
 			$user = $res->fetch_object();
-			if ( !$full && $user->emailpublic === '0' ) {
-				unset( $user->email );
+			if ( !$full ) {
+				if ( $user->emailpublic === '0' || $user->emailchecked === '0' ) {
+					unset( $user->email );
+				}
 				unset( $user->emailpublic );
+				unset( $user->emailchecked );
 			}
 		}
 		$res->free();
