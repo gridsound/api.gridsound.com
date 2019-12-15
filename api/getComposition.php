9255463359df3bdcd9f46bab07a5c19d0775688a
Thomas Tortorini <thomastortorini@gmail.com>
2019-12-15 05:59:48 +0100

    api/getComposition

---
+++ api/getComposition.php
@@ -0,0 +1,43 @@
+<?php
+
+error_reporting( -1 );
+
+require_once( 'common/sendJSON.php' );
+require_once( 'common/enableCors.php' );
+
+enableCors();
+
+$cmpId = $_GET[ 'id' ] ?? null;
+if ( !$cmpId ) {
+	sendJSON( 400, 'query:bad-format' );
+}
+
+require_once( 'common/connection.php' );
+require_once( 'common/getUser.php' );
+require_once( 'common/getComposition.php' );
+
+$cmp = getComposition( $mysqli, $cmpId );
+if ( $cmp === null ) {
+	sendJSON( 500, $mysqli->error );
+}
+if ( $cmp === false ) {
+	sendJSON( 404 );
+}
+
+session_start();
+$me = $_SESSION[ 'me' ] ?? null;
+$mine = $me && $cmp->iduser === $me->id;
+
+if ( $mine ) {
+	$user = $me;
+} else {
+	if ( $cmp->public === '0' ) {
+		sendJSON( 403 );
+	}
+	$user = getUser( $mysqli, 'id', $cmp->iduser );
+}
+
+sendJSON( 200, ( object )[
+	'user' => $user,
+	'composition' => $cmp,
+] );
