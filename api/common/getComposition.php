b43e0720de9fa3f52934105e2fb7f61209bef331
Thomas Tortorini <thomastortorini@gmail.com>
2019-12-24 03:58:52 +0100

    api/common/getComposition: return false if 404

--- api/common/getComposition.php
+++ api/common/getComposition.php
@@ -4,7 +4,7 @@ function getComposition( &$mysqli, $idcmp ) {
 	$cmp = null;
 	$res = $mysqli->query( "SELECT `id`, `iduser`, `public`, `data` FROM `compositions` WHERE `id` = '$idcmp'" );
 	if ( $res ) {
-		$cmp = $res->fetch_object();
+		$cmp = $res->fetch_object() ?? false;
 		$res->free();
 	}
 	return $cmp;
